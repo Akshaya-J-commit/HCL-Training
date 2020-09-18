4799b1687de53cb507dc69db3af649b1c50eced3
Akshaya-J-commit <68982797+Akshaya-J-commit@users.noreply.github.com>
2020-09-18 15:14:37 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,38 +1,25 @@
 package com.javatrain;
 class Add{
     public Add() {
+        System.out.println("a");
+    }
+   int  Add(int i)
+    {
+        return(i);
+    }
+
+}
+class Sub extends Add
+{
+    public int Sub(int i) {
+        super(i);
         System.out.println("hi");
     }
-    Add(int i)
-    {
-        System.out.println("hello");
-    }
-    Add(int i,int i1)
-    {
-        System.out.println("hi hello");
-    }
 }
-class Sub
-{
-    public Sub() {
-        System.out.println("akshu");
-    }
-    Sub(int i)
-    {
-        super(i);
-        System.out.println("swetha");
-    }
-   /* Sub(int i,int i1)
-    {
-        super(i,i1);
-        System.out.println("sister");
-    }
-}*/
 public class Main {
 
     public static void main(String[] args) {
-        Sub s=new Sub(10);
-
+	Sub s=new Sub(10);
 
     }
 }
