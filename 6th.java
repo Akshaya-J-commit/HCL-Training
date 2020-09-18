6b75b0d399431e682b6d61d970ad73c616871648
Akshaya-J-commit <68982797+Akshaya-J-commit@users.noreply.github.com>
2020-09-18 15:25:23 +0530

    Add files via upload

---
+++ 6th.java
@@ -0,0 +1,23 @@
+package com.hcl2;
+import java.util.Scanner;
+class ExtraType{
+    String name;
+    Long runs;
+    public void getName()
+    {
+        return name;
+    }
+    public
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+
